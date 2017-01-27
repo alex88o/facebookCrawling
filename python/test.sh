4db7ab24742f309d70636a08f3bbe9fcbd228ed1
alex88o <alessandro.ortis@gmail.com>
2017-01-27 16:34:13 +0100

    bug fix, automatic daily crawling

---
+++ python/test.sh
@@ -0,0 +1,36 @@
+python dailyAnalysis.py BarillaIT 2 > BarillaIT.out 2> BarillaIT.err
+python dailyAnalysis.py EsteeLauderUK 2 > EsteeLauderUK.out 2> EsteeLauderUK.err
+python dailyAnalysis.py PupaMilanoItaly 2 > PupaMilanoItaly.out 2> PupaMilanoItaly.err
+python dailyAnalysis.py MACcosmetics 2 > MACcosmetics.out 2> MACcosmetics.err
+python dailyAnalysis.py cliniqueuk 2 > cliniqueuk.out 2> cliniqueuk.err
+python dailyAnalysis.py 134045083337849 2 > 134045083337849.out 2> 134045083337849.err
+python dailyAnalysis.py 186732534837251 2 > 186732534837251.out 2> 186732534837251.err
+python dailyAnalysis.py 39461368738 2 > 39461368738.out 2> 39461368738.err
+python dailyAnalysis.py 229885990518845 2 > 229885990518845.out 2> 229885990518845.err
+python dailyAnalysis.py 235334306596863 2 > 235334306596863.out 2> 235334306596863.err
+python dailyAnalysis.py 143548395657271 2 > 143548395657271.out 2> 143548395657271.err
+
+python dailyAnalysis.py BarillaIT 2> BarillaIT.err
+python dailyAnalysis.py EsteeLauderUK 2> EsteeLauderUK.err
+python dailyAnalysis.py PupaMilanoItaly 2> PupaMilanoItaly.err
+python dailyAnalysis.py MACcosmetics > MACcosmetics.out 2> MACcosmetics.err
+python dailyAnalysis.py cliniqueuk 2> cliniqueuk.err
+python dailyAnalysis.py 134045083337849 2> 134045083337849.err
+python dailyAnalysis.py 186732534837251 2> 186732534837251.err
+python dailyAnalysis.py 39461368738 2> 39461368738.err
+python dailyAnalysis.py 229885990518845 2> 229885990518845.err
+python dailyAnalysis.py 235334306596863 2> 235334306596863.err
+python dailyAnalysis.py 143548395657271 2> 143548395657271.err
+
+
+python pastBehaviourAnalysis.py BarillaIT 2> BarillaIT.err
+python pastBehaviourAnalysis.py EsteeLauderUK 2> EsteeLauderUK.err
+python pastBehaviourAnalysis.py PupaMilanoItaly 2> PupaMilanoItaly.err
+python pastBehaviourAnalysis.py MACcosmetics 2> MACcosmetics.err
+python pastBehaviourAnalysis.py cliniqueuk 2> cliniqueuk.err
+python pastBehaviourAnalysis.py 134045083337849 2> 134045083337849.err
+python pastBehaviourAnalysis.py 186732534837251 2> 186732534837251.err
+python pastBehaviourAnalysis.py 39461368738 2> 39461368738.err
+python pastBehaviourAnalysis.py 229885990518845 2> 229885990518845.err
+python pastBehaviourAnalysis.py 235334306596863 2> 235334306596863.err
+python pastBehaviourAnalysis.py 143548395657271 2> 143548395657271.err
